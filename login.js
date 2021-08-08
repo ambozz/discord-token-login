97e97b3813b9576508643d719550541e66e283ec
ambossツ <74627962+ambozz@users.noreply.github.com>
2021-08-08 20:07:33 +0200

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,6 @@ setInterval(() => {
 document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
 }, 50);
 setTimeout(() => {
-location.reload();
+window.location.href = "https://discord.com/app";
 }, 2500);
 }
